7b335620cd99f497bf24bdbc587988233a66ff61
moksh <gmoksh985@gmail.com>
2026-03-22 22:57:02 +0530

    control flow

--- Learning-Js/Control Flow/controlflow.js
+++ Learning-Js/Control Flow/controlflow.js
@@ -72,10 +72,12 @@ switch (3){
         break;
 }
 
+
+
 function getval(val){
-    if(val<100) return "A";
-    else if(val<75) return "B";
+    if(val<25) return "D";
     else if(val<50) return "C";
-    else return "D";
+    else if(val<75) return "B";
+    else return "A";
 }
-console.log(getval(60));
+console.log(getval(80));
